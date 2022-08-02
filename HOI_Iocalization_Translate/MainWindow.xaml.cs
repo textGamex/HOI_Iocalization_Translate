3546c307ba4de4a79960c8d3c49b3bd344fd0f34
millennium <1543255825@qq.com>
2022-08-02 18:17:12 +0800

    添加项目文件。

---
+++ HOI_Iocalization_Translate/MainWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using HOI_Iocalization_Translate.Translation.Baidu;
+using HOI_Iocalization_Translate.Translation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Forms;
+using HOI_Iocalization_Translate.Translate;
+
+namespace HOI_Iocalization_Translate
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private string _modFolderPath;
+        
+        private List<TextData> _textDataList;
+
+        private void SelectFolderButton_Click(object sender, RoutedEventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            DialogResult result = dialog.ShowDialog();
+
+            if (result == System.Windows.Forms.DialogResult.Cancel)
+            {
+                return;
+            }
+
+            _modFolderPath = dialog.SelectedPath;
+            var path = GetLocalisationFolderPath(_modFolderPath);
+            DirectoryInfo dir = new DirectoryInfo(path);
+            List<FileInfo[]> fileInfos = new List<FileInfo[]>
+            {
+                dir.GetFiles()
+            };
+            //所有子文件夹的所有本地化文件
+            foreach (var d in dir.GetDirectories())
+            {
+                fileInfos.Add(d.GetFiles());
+            }
+
+            var textDataList = new List<TextData>();
+            foreach (FileInfo[] files in fileInfos)
+            {
+                foreach (var file in files)
+                {
+                    textDataList.Add(new TextData(file));
+                }
+            }
+            _textDataList = textDataList;
+        }
+
+        private static string GetLocalisationFolderPath(string modFolderPath)
+        {
+            return Path.Combine(modFolderPath, "localisation");
+        }
+
+       
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            TextBlack.Text = _api.GetTranslate("good\napple\nbad", "auto", "zh");
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (var data in _textDataList)
+            {
+                data.TranslateText(_api, "en");
+                Console.WriteLine($"{data.FileName} 完成");
+            }
+        }
+    }
+}
---
+++ HOI_Iocalization_Translate/Translate/Baidu/JsonTools.cs
@@ -0,0 +1,56 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+
+namespace HOI_Iocalization_Translate.Translation.Baidu
+{
+    public partial class BaiduTranslationApi
+    {
+        #region 解析 https://fanyi-api.baidu.com/api/trans/vip/translate 返回的json格式
+        /// <summary>
+        /// 辅助类
+        /// </summary>
+        private static class JsonTools
+        {
+            public const int NoError = 0;
+            public const int UnauthorizedUser = 52003;
+
+            public static string GetErrorMessage(in int errorCode)
+            {
+                switch (errorCode)
+                {
+                    case NoError: return "无错误";
+                    case UnauthorizedUser: return "未授权用户, 请检查ID或秘钥是否正确或者服务是否开通";
+                    case 52001: return "请求超时";
+                    case 52002: return "系统错误";
+                    case 54003: return "访问频率受限";
+                    case 54004: return "账户余额不足";
+                    default: return "未知错误";
+                }
+            }
+
+            public static Dictionary<string, string> GetTranslatedMap(string serverReturnedJson)
+            {
+                if (serverReturnedJson == null)
+                    throw new ArgumentNullException(nameof(serverReturnedJson));
+
+                Dictionary<string, string> data = new Dictionary<string, string>();
+                JObject rawJson = JObject.Parse(serverReturnedJson);
+                JToken translatedTextArray = rawJson.GetValue("trans_result");
+
+                foreach (JToken translated in translatedTextArray)
+                {
+                    JObject single = JObject.Parse(translated.ToString());
+
+                    string rawText = single.GetValue("src").ToString();
+                    string translatedText = single.GetValue("dst").ToString();
+                   
+                    data[rawText] = translatedText;                              
+                }
+                return data;
+            }
+        }
+    #endregion
+    }
+
+}
